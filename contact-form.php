7abbc40972d7b788783661ea072b5601dd352af4
danilo <dankusanovic>
2021-01-19 00:33:34 -0800

    the contact form was modified and  incorporated

---
+++ contact-form.php
@@ -0,0 +1,19 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'seismovlab.com';
+    $email_subject = "New Form Submission";
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+
+    $to = "dkusanov@caltech.edu";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: contact.html");
+?>
